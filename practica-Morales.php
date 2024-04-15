da1ac1bb27269bd6c4248e01f50717c7800bb68f
Dayro7 <dayro_13@msn.com>
2024-04-15 20:23:48 +0200

    Mi primer commit

---
+++ practica-Morales.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Este es un script de práctica para PHPDocumentor.
+ *
+ * @author Tu Nombre
+ * @version 1.0
+ */
+
+/**
+ * Esta función suma dos números.
+ *
+ * @param int $a Primer número a sumar.
+ * @param int $b Segundo número a sumar.
+ * @return int La suma de los dos números.
+ */
+function suma($a, $b) {
+    return $a + $b;
+}
+
+/**
+ * Esta función resta dos números.
+ *
+ * @param int $a El minuendo.
+ * @param int $b El sustraendo.
+ * @return int La resta de los dos números.
+ * @internal Solo para uso interno.
+ */
+function resta($a, $b) {
+    return $a - $b;
+}
+?>
